e8ffb13943000b35bc5d42ab3de5e8dfd9924451
Ebere Okezie <ebereokezie@gmail.com>
2022-10-04 19:27:39 -0500

    run first migration, create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,35 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+ToDo.create([
+  {
+    body: "Take out the trash",
+    day: "Sunday"
+  },
+  {
+    body: "Feed the Dog",
+    day: "Monday"
+  },
+  {
+    body: "Wash the Dishes",
+    day: "Tuesday"
+  },
+  {
+    body: "Do the Laundry",
+    day: "Wednesday"
+  },
+  {
+    body: "Buy Groceries",
+    day: "Thursday"
+  },
+  {
+    body: "Wash the car"
+    day: "Friday"
+  },
+  {
+    body: "Mow the Lawn"
+    day: "Saturday"
+  }
+
+])
 
 puts "✅ Done seeding!"
